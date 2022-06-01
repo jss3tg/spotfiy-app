82d807759e735b6b5efe2dfc34951ae16f67228d
jcqb09 <jcqb0909@gmail.com>
2022-06-01 13:59:08 -0400

    initial commit for API authorization

---
+++ api/routes/webapp.js
@@ -0,0 +1,63 @@
+const { urlencoded } = require("express");
+var express = require("express");
+var router = express.Router();
+//var fetch = require("node-fetch");
+const axios = require("axios");
+var dotenv = require("dotenv").config();
+
+const client_id = process.env.client_id;
+const client_secret = process.env.client_secret;
+const redirect_uri = "http://localhost:3000/";
+const scope = "user-top-read user-library-read";
+
+router.get("/", async (req, res, next) => {
+  try {
+    const url = new URL("https://accounts.spotify.com/authorize/");
+
+    url.searchParams.append("response_type", "code");
+    url.searchParams.append("client_id", client_id);
+    url.searchParams.append("scope", scope);
+    url.searchParams.append("redirect_uri", redirect_uri);
+    console.log(url);
+
+    res.status(200).json({ url: url });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+router.get("/callback", async (req, res, next) => {
+  try {
+    const code = req.query.code;
+
+    const url =
+      "https://accounts.spotify.com/api/token?grant_type=authorization_code&code=" +
+      code +
+      "&redirect_uri=" +
+      redirect_uri;
+    const headers = {
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret, "utf8").toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+    axios
+      .get(url, { method: "post", headers: headers })
+      .catch((err) => console.log(err))
+      .then((res) => res.json())
+      .then((data) => {
+        const obj = {
+          url: "http://localhost:3000/",
+          token: data.access_token,
+        };
+        return obj;
+      })
+      .then((obj) => res.json(obj));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [test, setTest] = useState();
+
+  const clickHandler = () => {
+    fetch("http://localhost:9000/webapp")
+      .then((res) => res.json())
+      .then((data) => {
+        window.open(data.url);
+      });
+  };
+
+  useEffect(() => {
+    fetch("/webapp/callback").then((res) => console.log(res));
+  }, [test]);
   return (
     <div className="App">
       <h1>Spotify App</h1>
+      <button onClick={() => clickHandler()}>Log In</button>
     </div>
   );
 }
